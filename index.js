61945c321713287869f846db75f0454457998c76
Nguyen Duy Minh Luan <luannguyen11998@gmail.com>
2022-08-04 18:36:08 +0700

    update1

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
+const bodyParser = require("body-parser");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.use("/public", express.static(`${process.cwd()}/public`));
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
+app.post("/api/shorturl", (req, res) => {
+  const original_url = req.body.url;
+
+  if (!original_url.match(/https:\/\/\w+\.\w+|http:\/\/\w+\.\w+/))
+    res.json({ error: "invalid url" });
+
+  res.json({ original_url });
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
