5585306bc1a8b33ff32ca8ac1495080f3d97325d
Nguyen Duy Minh Luan <luannguyen11998@gmail.com>
2022-08-04 20:59:35 +0700

    finished

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const bodyParser = require("body-parser");
+const dns = require("dns");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+let url = [];
 
 app.use(cors());
 
@@ -25,10 +27,19 @@ app.get("/api/hello", function (req, res) {
 app.post("/api/shorturl", (req, res) => {
   const original_url = req.body.url;
 
-  if (!original_url.match(/https:\/\/\w+\.\w+|http:\/\/\w+\.\w+/))
+  if (!original_url.match(/https:\/\/\w+.|http:\/\/\w+./))
     res.json({ error: "invalid url" });
 
-  res.json({ original_url });
+  if (!url.some((x) => x.orU == original_url))
+    url.push({ orU: original_url, shU: url.length + 1 });
+
+  const short_url = url.find((x) => x.orU == original_url).shU;
+
+  res.json({ original_url, short_url });
+});
+
+app.get("/api/shorturl/:short_url", (req, res) => {
+  res.redirect(url[req.params.short_url - 1].orU);
 });
 
 app.listen(port, function () {
